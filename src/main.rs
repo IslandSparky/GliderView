5fb4d9f081830f23e79b1738d50a9f1645a530e2
IslandSparky <geiselDD@gmail.com>
2023-03-10 14:53:49 -0800

    Correct date in license

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 /*Game of Life - gliderview
 
 MIT License
-Copyright (c) 202 Darwin Geiselbrecht
+Copyright (c) 2023 Darwin Geiselbrecht
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
